48cc92b4abf2d7aab38b2668a5a4d33795ae6e57
stevenacz <scoailazaa@gmail.com>
2021-06-28 18:35:55 -0500

    desestructuracion funcion

---
+++ src/ejercicios/06-desestructuracion-function.ts
@@ -0,0 +1,31 @@
+interface Producto {
+  desc: string;
+  precio: number;
+}
+
+const telefono: Producto = {
+  desc: 'Nokia A1',
+  precio: 150,
+};
+
+const tableta: Producto = {
+  desc: 'iPad A17',
+  precio: 350,
+};
+
+function calculaISV(productos: Producto[]): [number, number] {
+  let total = 0;
+
+  productos.forEach(({ precio }: Producto): void => {
+    total += precio;
+  });
+
+  return [total, total * 0.15];
+}
+
+const articulos = [telefono, tableta];
+
+const [total, isv] = calculaISV(articulos);
+
+console.log(`Total: ${total}`);
+console.log(`ISV: ${isv}`);
